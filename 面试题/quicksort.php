566c992c4fdfb5e0bdb65ab54cfd56581d630495
winslis2 <kunmingqaq@163.com>
2021-03-09 19:18:49 +0800

    快速排序

---
+++ 面试题/quicksort.php
@@ -0,0 +1,25 @@
+<?php
+
+$arr = [33, 24, 8, 21, 2, 23, 3, 32, 16];
+
+function quickSort($arr)
+{
+    if (count($arr)<2) {
+        return $arr;
+    }
+    $mid = $arr[0];
+    $left = $right = [];
+    //从1开始的
+    for($i=1;$i<count($arr);$i++) {
+        if ($mid<$arr[$i]) {
+            $right[] = $arr[$i];
+        }else{
+            $left[] = $arr[$i];
+        }
+    }
+    $left = quickSort($left);
+    $right = quickSort($right);
+    return array_merge($left,array($mid),$right);
+}
+
+print_r(quickSort($arr));
