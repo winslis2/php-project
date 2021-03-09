1341db542ffdf40995859a52470d3c712dfd0ab2
winslis2 <kunmingqaq@163.com>
2021-03-09 19:28:40 +0800

    单例模式

---
+++ 面试题/singleton.php
@@ -0,0 +1,23 @@
+<?php
+class Singleton {
+    private static $instance = null;
+    private function __construct()
+    {
+        return;
+    }
+    private function __clone() {
+        echo 'clone is not allowed';
+    }
+
+    public static function getInstance() {
+        if (self::$instance) {
+            return new self();
+        }else{
+            return self::$instance;
+        }
+    }
+}
+
+$singleton = Singleton::getInstance();
+$singleton2 = Singleton::getInstance();
+var_dump($singleton===$singleton2);
