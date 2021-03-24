b12d606e058c6e09b2e27fda245e3daf1455da26
winslis2 <kunmingqaq@163.com>
2021-03-24 23:44:47 +0800

    责任链模式

---
+++ designPattern/chainOfResponsibility.php
@@ -0,0 +1,37 @@
+<?php
+class board {
+    private $level = 1;
+    private $superior = 'admin';
+    public function process($level) {
+        if ($level <= $this->level) {
+            echo '版主删帖';
+        }else {
+            $superior = new $this->superior;
+            $superior->process($level);
+        }
+    }
+}
+
+class admin {
+    private $level = 2;
+    private $superior = 'police';
+    public function process($level) {
+        if ($level <= $this->level) {
+            echo '封禁账号';
+        }else {
+            $superior = new $this->superior;
+            $superior->process($level);
+        }
+    }
+}
+
+class police {
+    private $level = null;
+    private $superior = 'admin';
+    public function process($level) {
+       echo '抓起来';
+    }
+}
+$level = 2;
+$board = new board();
+$board->process($level);
